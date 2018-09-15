d507699856687e1b531aa420040c3a89ffaef5ea
zhouliujun <1096432931@qq.com>
2018-09-15 22:09:12 +0800

    新增readme

--- migu-superagent/app.js
+++ migu-superagent/app.js
@@ -5,15 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var miguRouter = require('./routes/migu');
+var musicRouter = require('./routes/music');
+
 
 var app = express();
-
-// view engine setup
-
-app.set('views', path.join(__dirname, 'views'));
-app.engine('.ejs', require('ejs').__express);
-app.set('view engine', 'ejs');
-
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,7 +17,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//默认页面
 app.use('/', indexRouter);
+//咪咕爬取
+app.use('/migu', miguRouter);
+//歌曲系统
+app.use('/music',musicRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
